5ea9b94dc9b9cb0c37a12837789719221ce9a30f
ege aydın <egeaydinn31@gmail.com>
2024-10-20 20:21:32 +0300

    .

---
+++ ReactTwoProject/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AppComponents from './Components/AppComponents'
+
+function App() {
+
+  return (
+    <>
+      <AppComponents/>
+    </>
+  )
+}
+
+export default App
---
+++ ReactTwoProject/src/Components/AppComponents.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+function AppComponents() {
+  return (
+    <>
+    <div className='form'>
+        <input type="text" placeholder='Name..' />
+        <input type="button" value="Gönder" />
+    </div>
+  </>
+  )
+}
+
+export default AppComponents
